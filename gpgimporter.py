1a1c90a2cd15fe064c1d40ad499ee12aed22bbf6
Zadjad Rezai <rzadjad+github@gmail.com>
2023-06-13 11:17:30 +0200

    Fix linting issues

--- gpgimporter.py
+++ gpgimporter.py
@@ -14,30 +14,63 @@ from bs4 import BeautifulSoup
 
 
 parser = argparse.ArgumentParser(
-                    prog = 'gpgimporter',
-                    description = 'Import and update all keys from any keyserver',
-                    epilog = 'Really simple and stupid')
+    prog="gpgimporter",
+    description="Import and update all keys from any keyserver",
+    epilog="Really simple and stupid",
+)
 
 
-parser.add_argument('-k', '--keyserver', help='Keyserver hostname without https:// or hkps:// prefix.')
-parser.add_argument('-l', '--lookup', help='Any string to filter the GPG list by.')
-parser.add_argument('-r', '--refresh', action='store_true')
+parser.add_argument(
+    "-k", "--keyserver", help="Keyserver hostname without https:// or hkps:// prefix."
+)
+parser.add_argument("-l", "--lookup", help="Any string to filter the GPG list by.")
+parser.add_argument("-r", "--refresh", action="store_true")
 
 options = parser.parse_args()
 
 if options.keyserver is None:
-    print('--keyserver must be provided')
+    print("--keyserver must be provided")
     sys.exit(1)
 
 if options.refresh:
     subprocess.run(
-        ["gpg", "--batch", "--keyserver", f"hkps://{options.keyserver}", "--refresh-keys"],
-        check=True
+        [
+            "gpg",
+            "--batch",
+            "--keyserver",
+            f"hkps://{options.keyserver}",
+            "--refresh-keys",
+        ],
+        check=True,
+    )
+    list_expired_keys = subprocess.run(
+        "gpg",
+        "--list-keys",
+        "|",
+        "grep -1 pub",
+        "|",
+        "sed 'N;s/\n/ /'",
+        "|",
+        "awk '/^pub.* \[expired\: / {id=$7; sub(/^.*\//, \"\", id); print id}'",
+        "|",
+        "fmt -w 999",
+    )
+    list_revoked_keys = subprocess.run(
+        "gpg",
+        "--list-keys",
+        "|",
+        "grep -1 pub",
+        "|",
+        "sed 'N;s/\n/ /'",
+        "|",
+        "awk '/^pub.* \[revoked\: / {id=$7; sub(/^.*\//, \"\", id); print id}'",
+        "|",
+        "fmt -w 999",
     )
     sys.exit(0)
 
 if options.lookup is None:
-    print('--lookup must be provided')
+    print("--lookup must be provided")
     sys.exit(1)
 
 lookup_url = f"/pks/lookup?search={options.lookup}&fingerprint=on&op=index"
@@ -50,14 +83,15 @@ all_fingerprints = []
 num_keys = 0
 
 gpg_sha_regex = re.compile(
-    r"(?P<hash>\b[0-9a-fA-F]{8}\b).*Fingerprint=(?P<fingerprint>\b[0-9a-fA-F ]+\b)", re.DOTALL
+    r"(?P<hash>\b[0-9a-fA-F]{8}\b).*Fingerprint=(?P<fingerprint>\b[0-9a-fA-F ]+\b)",
+    re.DOTALL,
 )
 
-for link in soup.find_all('pre'):
+for link in soup.find_all("pre"):
     m = re.search(gpg_sha_regex, link.text)
     try:
-        all_keys += m.group('hash') + " "
-        all_fingerprints.append(m.group('fingerprint'))
+        all_keys += m.group("hash") + " "
+        all_fingerprints.append(m.group("fingerprint"))
         num_keys += 1
     except AttributeError:
         print(f'warn: could not find a key in this line: "{link.text.strip()}"')
